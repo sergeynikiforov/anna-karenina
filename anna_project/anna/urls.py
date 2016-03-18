037898678df2028e53e23bfbcbe8317b5422ea5f
Sergey Nikiforov <serge.a.nikiforov@gmail.com>
2016-03-18 08:23:58 +0300

    add views, templates, form, basic search

--- anna_project/anna/urls.py
+++ anna_project/anna/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'anna'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^results/$', views.searchResults, name='results'),
 ]
--- anna_project/anna/views.py
+++ anna_project/anna/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Paragraph, Word
+from .forms import QueryForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the anna index.")
+    """
+    view for the index page of the app
+    """
+    form = QueryForm()
+    context = {'form': form}
+    return render(request, 'anna/index.html', context)
+
+
+def searchResults(request):
+    """
+    view for the results page
+    """
+    context = {}
+    query = request.GET.get('query')
+    form = QueryForm(request.GET)
+    context['form'] = form
+    if len(query) > 0:
+        # search in db
+        try:
+            obj = Word.objects.get(word=query.lower())
+            par_list =  obj.paragraphs #Paragraph.objects.order_by('id')[:5]
+            context['paragraphs'] = par_list
+        except Word.DoesNotExist:
+            print('Err: obj Word("{q}") does not exist'.format(q=query))
+
+    context['query'] = query
+
+    return render(request, 'anna/results.html', context)
